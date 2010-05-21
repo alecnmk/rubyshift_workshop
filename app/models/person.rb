ff9d563e103d8dc523115232e0b52025272f97db
Alexander Lockshyn <info@lab37.com>
2010-05-21 20:27:48 +0300

    Added basic AASM statuses

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,12 @@
 class Person < ActiveRecord::Base
+  include AASM
+  aasm_column :status
+  aasm_initial_state :pending
+  
+  aasm_state :pending
+  aasm_state :on_way
+  aasm_state :delivered
+  
   has_one :origin_station, :class => "Station", :foreign_key => "origin_station_id"
   has_one :destination_station, :class => "Station", :foreign_key => "destination_station_id"
   belongs_to :object, :polymorphic => true
--- app/models/train.rb
+++ app/models/train.rb
@@ -1,4 +1,13 @@
 class Train < ActiveRecord::Base
+  include AASM
+  aasm_column :status
+  aasm_initial_state :forward
+  
+  aasm_state :forward
+  aasm_state :backward
+
+  
+  
   has_many  :people, :as => :object
   belongs_to  :branch
   belongs_to  :station
