843ac9f3b432dff54a25d4905d2da352659c8aad
Alexander Lockshyn <info@lab37.com>
2010-05-21 21:00:13 +0300

    Added exit from station functionality

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,9 +1,18 @@
 class Station < ActiveRecord::Base
   has_many  :people, :as => :object
+  has_many  :delivered_people, :as => :object, :class_name => "Person", :foreign_key => "destination_station_id"
   belongs_to  :branch
   belongs_to :subway
 
   def update_state
     # load new people
+  end
+  
+  def exit_from_station
+    delivered = self.delivered_people.delivered
+    self.update_attribute :delta_out, self.delta_out + delivered.size
+    self.delivered_people.delivered.each do |person|
+      person.destroy
+    end
   end
 end
