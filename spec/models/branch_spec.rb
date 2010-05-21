0d64ccfc9ca0fdcf9113280cf7f5119a1e066001
Alexander Lockshyn <info@lab37.com>
2010-05-21 19:38:17 +0300

    Added empty models without real migrations

---
+++ spec/models/branch_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Branch do
+  before(:each) do
+    @valid_attributes = {
+      
+    }
+  end
+
+  it "should create a new instance given valid attributes" do
+    Branch.create!(@valid_attributes)
+  end
+end
