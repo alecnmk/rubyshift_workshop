7f496b01a18c3b3981e9eaaaccbebd25b8e0eeed
Ruslan <rbondarev@gmail.com>
2010-05-21 20:52:45 +0300

    update game time on heartbeat

--- app/models/subway.rb
+++ app/models/subway.rb
@@ -4,6 +4,7 @@ class Subway < ActiveRecord::Base
   has_many  :people
 
   def heartbeat
+    self.update_attribute(:last_updated_at, self.last_updated_at + 1.minute)
     self.trains.each(&:update_state)
     self.branches.each do |branch|
       branch.stations.each(&:update_state)
